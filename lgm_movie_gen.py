d27ec9724e44726a4397a840ad8038004f0d0580
Tom Nicholson <tfwnicholson+1@gmail.com>
2020-03-26 19:22:53 +0900

    Pretty pictures

--- lgm_movie_gen.py
+++ lgm_movie_gen.py
@@ -1,8 +1,12 @@
+import math
 import itertools
+import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 
 np.random.seed(1000007)
+matplotlib.rcParams.update({'font.size': 22})
 
 def generate_impulses(length, min_impulse, max_impulse):
     return np.random.uniform(min_impulse, max_impulse, length)
@@ -45,7 +49,7 @@ observed = np.array(list(map(observe, hidden)))
 
 # kalman filter prediction model
 F_k = 1.0
-Q_k = 1.0
+Q_k = 0.1
 def predict_state(x_km1_km1):
     """
     a-priori state estimation
@@ -58,7 +62,7 @@ def predict_covariance(P_km1_km1):
     return F_k * P_km1_km1 * F_k + Q_k
 
 H_k = 1.0
-R_k = 1.0
+R_k = 0.5
 def innovation_residual(z_k, x_km1_km1):
     return z_k - H_k * predict_state(x_km1_km1)
 def innovation_covariance(P_km1_km1):
@@ -86,21 +90,67 @@ P_0 = 2             # unit variance FTW!
 infered_states, infered_vars = zip(*itertools.accumulate(observed, kalman_step, initial=(x_0, P_0)))
 infered_states = np.array(infered_states)
 infered_vars = np.array(infered_vars)
+error_bars = 2 * np.sqrt(infered_vars)
 
 # plot
 
-plt.subplot(2, 1, 1)
+AUV_pos_x = 6
+AUV_pos_y = -2.5
+
+def calc_circle_intersect(cxy, r, xy):
+    """
+    :param cxy: tuple of center of circle
+    :param r: radius of circle
+    :param xy: point to intercept from
+    """
+    cx, cy = cxy
+    x, y = xy
+    dx = x - cx
+    dy = y - cy
+
+    theta = math.atan(dy / dx)
+
+    px = cx + r *  math.cos(theta)
+    py = cy + r *  math.sin(theta)
+
+    return px, py
+
+ax = plt.subplot(2, 1, 1)
 true_path, = plt.plot(hidden, c='b')
 observed_path, = plt.plot(observed, c='r')
-AUV, = plt.plot(5.0, -3.0, marker='x', markersize=10, color="green")
-plt.legend([true_path, observed_path, AUV], ['True path', 'Observed path', 'AUV'])
 
-plt.subplot(2, 1, 2)
+# plot position 5 estimate
+plt.plot(4, hidden[4], marker='+', markersize=15, color="b", markeredgewidth=3)
+assumed_position, = plt.plot(4, observed[4], marker='+', markersize=15, color="m", markeredgewidth=3)
+AUV, = plt.plot(AUV_pos_x, AUV_pos_y, marker='x', markersize=10, color="green", markeredgewidth=3)
+plt.legend([true_path, observed_path, AUV, assumed_position],
+           ['True path', 'Observed path', 'AUV', 'Safest guess'])
+ax.title.set_text("Raw sensor data")
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+
+ax = plt.subplot(2, 1, 2)
 true_path, = plt.plot(hidden, c='b')
 guessed_path, = plt.plot(infered_states, c='r')
-safety_margin, = plt.plot(infered_states + infered_vars, 'r--')
-plt.plot(infered_states - infered_vars, 'r--')
-AUV = plt.plot(5.0, -3.0, marker='x', markersize=10, color="green")
-plt.legend([true_path, guessed_path, safety_margin, AUV], ['True path', 'Guessed path', 'Safety margin', 'AUV'])
 
+
+# plot position 5 estimate
+circle_x = 4.0
+circle_y = infered_states[4]
+radius = error_bars[4]
+guessed_x, guessed_y = calc_circle_intersect((circle_x, circle_y), radius, (AUV_pos_x, AUV_pos_y))
+plt.plot(4, hidden[4], marker='+', markersize=15, color="b", markeredgewidth=3)
+assumed_position, = plt.plot(guessed_x, guessed_y, marker='+', markersize=15, color="m", markeredgewidth=3)
+
+ax.add_artist(plt.Circle((4.0, infered_states[4]), radius=error_bars[4], color="red", alpha=0.5))
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+
+safety_margin, = plt.plot(infered_states + error_bars, 'r--')
+plt.plot(infered_states - error_bars, 'r--')
+AUV, = plt.plot(AUV_pos_x, AUV_pos_y, marker='x', markersize=10, color="green", markeredgewidth=3)
+plt.legend([true_path, guessed_path, safety_margin, AUV, assumed_position],
+           ['True path', 'Guessed path', 'Safety margin', 'AUV', 'Safest guess'])
+
+ax.title.set_text("Filtered data")
 plt.show()
