b6672c09ac59ee256d77b21bf30811bc237d5774
Tom N <tfwnicholson+1@gmail.com>
2020-03-30 18:48:43 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Greg Ward',
+      author_email='gward@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=[matplotlib==3.2.1
+                numpy==1.18.2]
+     )
